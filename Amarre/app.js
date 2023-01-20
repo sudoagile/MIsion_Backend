282ead2e9da3d6eee0528a01303e00f0a8ae06b4
sudoagile <98191653+sudoagile@users.noreply.github.com>
2023-01-20 17:41:08 -0500

    Amarre ok

---
+++ Amarre/app.js
@@ -0,0 +1,23 @@
+function moverPosiciónRandom(elm){
+elm.style.position = 'absolute';
+elm.style.top = Math.random() * (window.innerHeight - elm.offsetHeight) + 'px';
+elm.style.left = Math.random() * (window.innerWidth - elm.offsetWidth) + 'px';
+}
+
+let btnSi = document.getElementById("btn_si");
+let btnNo = document.getElementById("btn_no");
+let divModoSexo = document.getElementsByClassName("modo_sexo")[0];
+
+btnNo.addEventListener('mouseenter',function(e){moverPosiciónRandom(e.target)});
+
+btnSi.addEventListener('click',function(e){
+
+    alert('Sabía que dirías que SÍ. Casemonos ya y tengamos hijos. TE AMO!!!!' )
+
+    divModoSexo.style.display = "block";
+    const cancion = new Audio("img/img_modo_hot.mp3");
+    cancion.play();
+
+});
+
+

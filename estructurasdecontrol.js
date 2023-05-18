b1ca1f45bdb310785eaf5fc5cb6b277c39ed32dc
sudoagile <98191653+sudoagile@users.noreply.github.com>
2023-05-17 21:56:40 -0500

    Update estructurasdecontrol.js

--- estructurasdecontrol.js
+++ estructurasdecontrol.js
@@ -22,3 +22,34 @@ console.log(var5);
 console.log(var6+var6);
 var6 = 5;
 console.log(var6);
+
+console.log("____________________________________")
+//If else
+
+function validarEdad(edad){
+    if(confirm("¿Estas de acuerdo con seguir con esto?")){
+
+        console.log("Sigue bajo tu propio riesgo");
+        if(edad >= 18 && edad <= 150){
+        console.log("Puedes comprar cigarrillos");
+        } else if(edad>150) {
+            console.log("Saludame a Cepillin")
+        }else if (edad >=0){
+            console.log("Tas muy chiquito, vuelve en unos años")
+        }else{
+            console.log("A caray, tu no deberias existir")
+        }
+    } else{
+        console.log("Que miedoso...xD")
+    }
+}
+// If else
+let edad = prompt("Introduce tu edad");
+
+if(edad && !isNaN(edad)){
+    validarEdad(edad)
+} else {
+    alert("Debes introducir tu edad");
+    location.reload();
+}
+
